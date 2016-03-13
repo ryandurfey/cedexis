95e70f503a2b3b77ea744234dc2124448a69e08b
Ryan Durfey <rdurfey@gmail.com>
2016-03-13 11:27:45 -0600

    working copy

--- cran_agg_processor.py
+++ cran_agg_processor.py
@@ -1,32 +1,36 @@
 
 import ipaddress #convert IPs to integers
 import csv #read in csv files
-import base64 #convert via header
-import os
-from pathlib import Path
 
-filename = "/Users/RDURFE200/Documents/cedexis/cran_aggs.txt"
+
+filename = "/Users/RDURFE200/Documents/cedexis/lookups/cran_aggs.txt"
 with open(filename) as temp_file:
     reader = csv.reader(temp_file, delimiter='\t')
     ca = list(reader)
 temp_file.close()
+print(ca[1])
+
 
 for i, val in enumerate(ca):
-    #split cider block
-    #convert block to integer
-    #If IP address is ipv4,
-        # convert to start integer
-        # add the cidr block to end integer
-    #Elif IP address is ipv6
-        # convert to start integer
-        # add the cidr block to end integer
-    #else:
-        #start num = 0
-        #end num = 0
-    #append value to ca
+    ip, block = ca[i][1].split('/') #split the cidr block
+    block=int(block) #convert block to integer from string
+    ip = ipaddress.ip_address(ip)
+    if ip.version == 4 : # convert address from ipv4 notation to integer
+        block_start_int = int(ipaddress.IPv4Address(ip))
+        block_end_int = block_start_int + 2**(32 - block) - 1
+    elif ip.version == 6: #convert address from ipv6 notation to integer
+        block_start_int = int(ipaddress.IPv6Address(ip))
+        block_end_int = block_start_int + 2**(128 - block) - 1
+    else:
+        block_start_int = 0
+        block_end_int = 0
+    #Append data to a list of dictionaries so it can be sorted and processed
+    ca[i].append(block_start_int)
+    ca[i].append(block_end_int)
 
-filename = "/Users/RDURFE200/Documents/cedexis/cran_aggs_processed.txt"
+
+filename = "/Users/RDURFE200/Documents/cedexis/lookups/cran_aggs_processed.txt"
 with open(filename, "w") as temp_file:
-    writer = csv.writer(temp_file)
+    writer = csv.writer(temp_file, delimiter ='\t')
     writer.writerows(ca)
 temp_file.close()
